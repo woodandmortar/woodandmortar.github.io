93f2fd3b3cd080a24d55ef01e584820eb73fc03e
Micheal <Micheal.mfg@gmail.com>
2019-05-26 12:55:44 -0500

    finished

--- js/upgrade.js
+++ js/upgrade.js
@@ -288,6 +288,28 @@
             commandReset(9);
             return
           },
+          "clockin" : function(){
+
+            $.ajax({
+              url: "https://docs.google.com/forms/d/e/1FAIpQLSdvXT57YthGUIFqmd_4yFu8Wm1QkkVgFO7kf3DLTdTmdvRVVg/formResponse",
+              data:  { "entry.569412235" : task , "entry.320157692" : project , "entry.1143322173" : location },
+              type: "POST",
+              dataType: "xml",
+              statusCode: {
+                0: function () {
+        //					alert("Click -OK- to submit message");
+                },
+                200: function () {
+        //					alert("Click -OK- to submit message");
+                }
+                    }
+            });
+                setTimeout(function(){
+                  window.location.href = 'https://woodandmortar.github.io/clockedin.html';
+                }, 2500);
+            commandReset(10);
+            return
+          },
 
 
         }

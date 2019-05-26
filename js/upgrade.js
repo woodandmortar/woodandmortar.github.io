c7060f9ae29954be5c5ff92f6427ad55464e7bf7
Micheal <Micheal.mfg@gmail.com>
2019-05-26 12:22:55 -0500

    tisk tisk

--- js/upgrade.js
+++ js/upgrade.js
@@ -316,31 +316,6 @@ var project;
             commandReset(8);
             return
           },
-          "clockin" : function(){
-            document.getElementById("main-contact-form").action = "https://docs.google.com/forms/d/e/1FAIpQLSdvXT57YthGUIFqmd_4yFu8Wm1QkkVgFO7kf3DLTdTmdvRVVg/formResponse";
-            var project = document.getElementById('vresp2').innerHTML;
-            var location = document.getElementById('vresp').innerHTML;
-            var task = document.getElementById('vresp3').innerHTML;
-
-              $.ajax({
-                url: "https://docs.google.com/forms/d/e/1FAIpQLSdvXT57YthGUIFqmd_4yFu8Wm1QkkVgFO7kf3DLTdTmdvRVVg/formResponse",
-                data:  { "entry.569412235" : task , "entry.320157692" : project , "entry.1143322173" : location },
-                type: "POST",
-                dataType: "xml",
-                statusCode: {
-                  0: function () {
-            //					alert("Click -OK- to submit message");
-                  },
-                  200: function () {
-            //					alert("Click -OK- to submit message");
-                  }
-                      }
-              });
-                  setTimeout(function(){
-                    window.location.href = 'https://woodandmortar.github.io/clockedin.html';
-                  }, 2500);
-            },
-          // work experience
         }
 
         var reactionInput = {

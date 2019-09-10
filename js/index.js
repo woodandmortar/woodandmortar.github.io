cadfdacdeb7c98b7c063623980ca5b426288bcc3
Micheal <Micheal.mfg@gmail.com>
2019-09-10 13:07:11 -0500

    total overhaul
    here

--- js/index.js
+++ js/index.js
@@ -27,10 +27,60 @@ $(function(){
       setTimeout(function(){
 
       },800);
-    
 
-    
+
+
     console.log('current '+current);
     console.log('prev '+prev);
   }
 });
+
+
+
+
+jQuery(document).ready(function($){
+  setTimeout(function(){ showNewsletterPopup(); }, 1200);
+
+  $('.popup-close').click(function(){
+      $('.newsletter-overlay').hide();
+      setCookie('newsletter-popup', 'popped', 30);
+  });
+});
+
+function showNewsletterPopup(){
+  if(getCookie('newsletter-popup') == ""){
+     $('.newsletter-overlay').show();
+     setCookie('newsletter-popup', 'popped', 30);
+  }
+  else{
+    console.log("Newsletter popup blocked.");
+  }
+}
+
+
+function setCookie(cname,cvalue,exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname+"="+cvalue+"; "+expires+"; path=/";
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++)
+    {
+        var c = jQuery.trim(ca[i]);
+        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
+
+        function togglediv(id) {
+    var div = document.getElementById(id);
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}

d2be631c92df562b9393ad96df96355b245a0031
Micheal Salmon <micheal.mfg@gmail.com>
2020-04-16 19:16:45 -0500

    Mortons Here!!!
    Bum ba da DUMM!!!!

---
+++ js/animate.js
@@ -0,0 +1,60 @@
+
+
+if(document.getElementById)
+document.write('<style type="text/css">.fins {float:left;display:none;}#zero {display:block;}<\/style>');
+
+function reveal(det){
+if(!document.getElementById) return;
+if (!document.getElementsByClassName){
+document.getElementsByClassName = function(cn){
+cn = cn.replace(/ +/g, ' ').split(' ');
+var ar = [], testname = function(n){
+for (var re, i = cn.length - 1; i > -1; --i){
+re = new RegExp('(^|\W)' + cn[i] + '(\W|$)');
+if(!re.test(n)) return false;
+}
+return true;
+}
+for(var d = document.all || document.getElementsByTagName('*'), i = 0; i < d.length; ++i)
+if(testname(d[i].className))
+ar[ar.length] = d[i];
+return ar;
+};
+document.getElementsByClassName.spoof = true;
+}
+for (var d = document.getElementsByClassName('fins'), i = d.length - 1; i > -1; --i)
+d[i].style.display = 'none';
+document.getElementById(det).style.display = 'block';
+if (document.getElementsByClassName.spoof)
+document.getElementsByClassName.spoof = document.getElementsByClassName = null;
+}
+
+
+function randomize(collection) {
+  var randomNumber = Math.floor(Math.random() * collection.length);
+  return collection[randomNumber]
+}
+
+function confetti() {
+  var $confettiItems = $('<div class="confetti"></div>'),
+  colors = [
+      '#E75624',
+      '#E9128C'
+  ],
+  height = 6.6,
+  width = 6.6;
+
+  var scale, $confettiItem;
+
+  for(var i=0; i<100; i++) {
+    scale = Math.random() * 1.75 + 1;
+    $confettiItem = $('<svg class=\'confetti-item\' width=\'' + width * scale + '\' height=\'' + height * scale + '\' viewbox=\'0 0 ' + width + ' ' + height + '\'>\n  <use transform=\'rotate(' + Math.random() * 360 + ', ' + width / 2 + ', ' + height / 2 + ')\' xlink:href=\'#svg-confetti\' />\n</svg>');
+    $confettiItem.css({
+      'animation': Math.random() + 2 + 's ' + Math.random() * 2 + 's confetti-fall ease-in both',
+      'color': randomize(colors),
+      'left': Math.random() * 100 + 'vw'
+    });
+    $confettiItems.append($confettiItem);
+  }
+  $('body').append($confettiItems)
+}

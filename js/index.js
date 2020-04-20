558015e7ef4088e85b27cf24e401179b8535e91e
woodandmortar <moreforblack@gmail.com>
2020-04-20 03:41:22 -0500

    backdate

--- js/index.js
+++ js/index.js
@@ -50,17 +50,3 @@ jQuery(document).ready(function($){
     var div = document.getElementById(id);
     div.style.display = div.style.display == "none" ? "block" : "none";
 }
-
-
-var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-
-  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-    requestFullScreen.call(docEl);
-  }
-  else {
-    cancelFullScreen.call(doc);
-  }

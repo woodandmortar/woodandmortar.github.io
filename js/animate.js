2e8c4cbb32357db5c979ba31ab8021eec98e9c03
woodandmortar <moreforblack@gmail.com>
2020-04-23 19:47:50 -0500

    adding click game
    sleep vs smiley micheal

--- js/animate.js
+++ js/animate.js
@@ -1,35 +1,5 @@
 
 
-if(document.getElementById)
-document.write('<style type="text/css">.fins {float:left;display:none;}#zero {display:block;}<\/style>');
-
-function reveal(det){
-if(!document.getElementById) return;
-if (!document.getElementsByClassName){
-document.getElementsByClassName = function(cn){
-cn = cn.replace(/ +/g, ' ').split(' ');
-var ar = [], testname = function(n){
-for (var re, i = cn.length - 1; i > -1; --i){
-re = new RegExp('(^|\W)' + cn[i] + '(\W|$)');
-if(!re.test(n)) return false;
-}
-return true;
-}
-for(var d = document.all || document.getElementsByTagName('*'), i = 0; i < d.length; ++i)
-if(testname(d[i].className))
-ar[ar.length] = d[i];
-return ar;
-};
-document.getElementsByClassName.spoof = true;
-}
-for (var d = document.getElementsByClassName('fins'), i = d.length - 1; i > -1; --i)
-d[i].style.display = 'none';
-document.getElementById(det).style.display = 'block';
-if (document.getElementsByClassName.spoof)
-document.getElementsByClassName.spoof = document.getElementsByClassName = null;
-}
-
-
 function randomize(collection) {
   var randomNumber = Math.floor(Math.random() * collection.length);
   return collection[randomNumber]

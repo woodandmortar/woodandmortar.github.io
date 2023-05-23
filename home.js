5dd59841132da07ab20e22493f81528cc5d5903a
woodandmortar <moreforblack@gmail.com>
2023-05-23 12:06:07 -0500

    home update
    Task manager

--- home.js
+++ home.js
@@ -1,19 +1,33 @@
-$('.mail-choice').change(function() {
-    if($(this).is(":checked")) {
-        $(this).parent().addClass('selected-bg');
-  } else {
-    $(this).parent().removeClass('selected-bg');
-  }
-});
+document.addEventListener('DOMContentLoaded', function () {
+  var modeSwitch = document.querySelector('.mode-switch');
 
-const colorInput = document.getElementById("colorpicker");
-
-colorInput.addEventListener("input", (e) => {
- document.body.style.setProperty("--button-color", e.target.value);
-});
-
-$('.inbox-calendar').click(function(){
-  $('.calendar-container').toggleClass('calendar-show');
- $('.inbox-container').toggleClass('hide');
- $('.mail-detail').toggleClass('hide'); 
+  modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+    modeSwitch.classList.toggle('active');
+  });
+  
+  var listView = document.querySelector('.list-view');
+  var gridView = document.querySelector('.grid-view');
+  var projectsList = document.querySelector('.project-boxes');
+  
+  listView.addEventListener('click', function () {
+    gridView.classList.remove('active');
+    listView.classList.add('active');
+    projectsList.classList.remove('jsGridView');
+    projectsList.classList.add('jsListView');
+  });
+  
+  gridView.addEventListener('click', function () {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    projectsList.classList.remove('jsListView');
+    projectsList.classList.add('jsGridView');
+  });
+  
+  document.querySelector('.messages-btn').addEventListener('click', function () {
+    document.querySelector('.messages-section').classList.add('show');
+  });
+  
+  document.querySelector('.messages-close').addEventListener('click', function() {
+    document.querySelector('.messages-section').classList.remove('show');
+  });
 });

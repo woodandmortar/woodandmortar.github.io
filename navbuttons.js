e2ffb0bbee55c01d5a1b1c50ac88f155983cb4e3
woodandmortar <moreforblack@gmail.com>
2023-08-04 10:46:27 -0500

    update to version 4.1 stable
    fully working

---
+++ navbuttons.js
@@ -0,0 +1,95 @@
+
+function energyAuditActive(){
+  document.getElementById("energyAudit").style.display = "block";
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  document.getElementById("salmon").style.display = "none";
+  navAudit.classList.add("active");
+  navHome.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navSettings.classList.remove("active");
+  navShopping.classList.remove("active");
+  navSalmon.classList.remove("active");
+}
+
+function scheduleActive(){
+  document.getElementById("schedule").style.display = "block";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  document.getElementById("salmon").style.display = "none";
+  navHome.classList.remove("active");
+  navSchedule.classList.add("active");
+  navAudit.classList.remove("active");
+  navSettings.classList.remove("active");
+  navShopping.classList.remove("active");
+  navSalmon.classList.remove("active");
+}
+
+function splashActive(){
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "block";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  document.getElementById("salmon").style.display = "none";
+  navHome.classList.add("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navSettings.classList.remove("active");
+  navShopping.classList.remove("active");
+  navSalmon.classList.remove("active");
+}
+
+function settingsActive(){
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  document.getElementById("salmon").style.display = "none";
+  navSettings.classList.add("active");
+  navHome.classList.remove("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navShopping.classList.remove("active");
+  navSalmon.classList.remove("active");
+}
+
+function shoppingActive(){
+  document.getElementById("shopping").style.display = "block";
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("salmon").style.display = "none";
+  navShopping.classList.add("active");
+  navSettings.classList.remove("active");
+  navHome.classList.remove("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navSalmon.classList.remove("active");
+}
+
+function salmonActive(){
+  document.getElementById("salmon").style.display = "block";
+  document.getElementById("shopping").style.display = "none";
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  navSalmon.classList.add("active");
+  navShopping.classList.remove("active");
+  navSettings.classList.remove("active");
+  navHome.classList.remove("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+}

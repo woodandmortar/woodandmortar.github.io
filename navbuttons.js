c3218010c982f7d40a88758f8202a71a3cec8256
woodandmortar <144293908+LuminaFieldsNZ@users.noreply.github.com>
2023-12-17 00:43:34 -0600

    revert
    way back

---
+++ navbuttons.js
@@ -0,0 +1,69 @@
+
+function energyAuditActive(){
+  document.getElementById("energyAudit").style.display = "block";
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  navAudit.classList.add("active");
+  navHome.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navSettings.classList.remove("active");
+  navShopping.classList.remove("active");
+}
+
+function scheduleActive(){
+  document.getElementById("schedule").style.display = "block";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  navHome.classList.remove("active");
+  navSchedule.classList.add("active");
+  navAudit.classList.remove("active");
+  navSettings.classList.remove("active");
+  navShopping.classList.remove("active");
+}
+
+function splashActive(){
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "block";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  navHome.classList.add("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navSettings.classList.remove("active");
+  navShopping.classList.remove("active");
+}
+
+function settingsActive(){
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "block";
+  document.getElementById("schedule").style.display = "none";
+  document.getElementById("shopping").style.display = "none";
+  navSettings.classList.add("active");
+  navHome.classList.remove("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+  navShopping.classList.remove("active");
+}
+
+function shoppingActive(){
+  document.getElementById("shopping").style.display = "block";
+  document.getElementById("secretDiv").style.display = "none";
+  document.getElementById("energyAudit").style.display = "none";
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("settingsMain").style.display = "none";
+  document.getElementById("schedule").style.display = "none";
+  navShopping.classList.add("active");
+  navSettings.classList.remove("active");
+  navHome.classList.remove("active");
+  navAudit.classList.remove("active");
+  navSchedule.classList.remove("active");
+}
